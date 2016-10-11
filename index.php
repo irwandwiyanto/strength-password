04aa387fd7dd57d52fa9576a357829a8f0d2eb60
irwan dwiyanto <irwan.develop@gmail.com>
2016-10-11 14:10:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+	<title> Passqord strength</title>
+<head>	
+	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.4.min.js"></script>
+</head>
+<body>
+	<input type="password" name="pass" id="pass" /><br>
+	<span id="passstrength"></span>
+</body>
+</html>
